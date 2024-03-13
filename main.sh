f31fefc88f8fd9cd95d76e8cd38febf4657835b1
Jigar R Rathod <136077334+jrrathod1505@users.noreply.github.com>
2024-03-14 01:44:48 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome to Forensics and Malware Analysis Script!"
+
+# Function to display the main menu
+function display_menu () {
+  echo "Select an option:"
+  echo "1) Memory Forensics with Volatility"
+  echo "2) Rootkit Detection"
+  echo "3) Scan a File for Malware"
+  echo "4) Exit"
